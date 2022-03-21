960883d90f77bed05719063107a850e22343984b
thong2802 <ducthong2802@gmail.com>
2022-03-21 16:28:39 +0700

    commit nop bai luc 16h30 ngay 21/3/2022

---
+++ 1911065673_NguyenDucThong_BigSchool/Migrations/202203210912038_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace _1911065673_NguyenDucThong_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(1, 'Delopment')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(2, ' Bessiness')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(3, 'Markting')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
