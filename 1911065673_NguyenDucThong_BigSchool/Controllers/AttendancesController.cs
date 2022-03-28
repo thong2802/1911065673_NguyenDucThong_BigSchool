299680f02d8d9dd6afc41c1b047f19c38bb869c9
thong2802 <ducthong2802@gmail.com>
2022-03-28 11:49:59 +0700

    Commit ngay 28/3/2022 luc 11 h 49

---
+++ 1911065673_NguyenDucThong_BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,39 @@
+﻿using _1911065673_NguyenDucThong_BigSchool.DTOs;
+using _1911065673_NguyenDucThong_BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1911065673_NguyenDucThong_BigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+      
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attdanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attdanceDto.CourseId))
+                return BadRequest("The Attendance Alrealy exists !");
+            var attendance = new Attendance
+            {
+                CourseId = attdanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
