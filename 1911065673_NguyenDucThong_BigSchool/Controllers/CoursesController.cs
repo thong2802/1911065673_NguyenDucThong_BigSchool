caa011670d19e2327b28bc00f9a2ada57710feb7
thong2802 <ducthong2802@gmail.com>
2022-03-28 10:35:36 +0700

    Commit ngay 28/3/2022 luc 10 h35

--- 1911065673_NguyenDucThong_BigSchool/Controllers/CoursesController.cs
+++ 1911065673_NguyenDucThong_BigSchool/Controllers/CoursesController.cs
@@ -1,17 +1,158 @@
-﻿using System;
+﻿using _1911065673_NguyenDucThong_BigSchool.Models;
+using _1911065673_NguyenDucThong_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
 namespace _1911065673_NguyenDucThong_BigSchool.Controllers
 {
-    public class CoursesController : Controller
-    {
-        // GET: Courses
-        public ActionResult Create()
+        [Authorize]
+        public class CoursesController : Controller
         {
-            return View();
-        }
+            private readonly ApplicationDbContext _dbContext;
+            public CoursesController()
+            {
+                _dbContext = new ApplicationDbContext();
+                CultureInfo culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+                culture.DateTimeFormat.ShortDatePattern = "dd-MMM-yyyy";
+                culture.DateTimeFormat.LongTimePattern = "";
+                Thread.CurrentThread.CurrentCulture = culture;
+            }
+            //
+            // GET: /Courses/Create
+            public ActionResult Create()
+            {
+                var viewModel = new CourseViewModel()
+                {
+                    Categories = _dbContext.Categories.ToList(),
+                    Heading = "Add Course"
+                };
+                return View(viewModel);
+            }
+            //
+            // POST: /Courses/Create
+            [Authorize]
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(CourseViewModel viewModel)
+            {
+                if (!ModelState.IsValid)
+                {
+                    viewModel.Categories = _dbContext.Categories.ToList();
+                    return View("Create", viewModel);
+                }
+                var course = new Course()
+                {
+                    LecturerId = User.Identity.GetUserId(),
+                    DateTime = viewModel.GetDateTime(),
+                    CategoryId = viewModel.Category,
+                    Place = viewModel.Place
+                };
+                _dbContext.Courses.Add(course);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+        /*
+                    [Authorize]
+                    public ActionResult Attending()
+                    {
+                        var userId = User.Identity.GetUserId();
+                        var courses = _dbContext.Attendances
+                            .Where(a => a.AttendeeId == userId)
+                            .Select(a => a.Course)
+                            .Include(l => l.Lecturer)
+                            .Include(l => l.Category)
+                            .Where(a => a.IsCanceled == false)
+                            .ToList();
+
+                        var viewModel = new CoursesViewModel
+                        {
+                            UpcommingCourses = courses,
+                            ShowAction = User.Identity.IsAuthenticated
+                        };
+
+                        return View(viewModel);
+                    }
+                    public ActionResult Following()
+                    {
+                        var userId = User.Identity.GetUserId();
+                        var followings = _dbContext.Followings
+                            .Where(a => a.FolloweeId == userId)
+                            .Select(a => a.Follower)
+                            .ToList();
+
+                        var viewModel = new FollowingViewModel
+                        {
+                            Followings = followings,
+                            ShowAction = User.Identity.IsAuthenticated
+                        };
+
+                        return View(viewModel);
+                    }
+
+
+                    public ActionResult Mine()
+                    {
+                        var userId = User.Identity.GetUserId();
+                        var courses = _dbContext.Courses
+                            .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                            .Include(l => l.Lecturer)
+                            .Include(c => c.Category)
+                            .ToList();
+
+                        return View(courses);
+                    }
+
+                    [Authorize]
+                    public ActionResult Edit(int id)
+                    {
+                        var userId = User.Identity.GetUserId();
+
+                        var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+
+                        var viewModel = new CourseViewModel
+                        {
+                            Categories = _dbContext.Categories.ToList(),
+                            Date = course.DateTime.ToString("dd/M/yyyy"),
+                            Time = course.DateTime.ToString("HH:mm"),
+                            Category = course.CategoryId,
+                            Place = course.Place,
+                            Heading = "Edit Course",
+                            Id = course.Id
+                        };
+
+                        return View("Create", viewModel);
+
+                    }
+                    [Authorize]
+                    [HttpPost]
+                    [ValidateAntiForgeryToken]
+                    public ActionResult Update(CourseViewModel viewModel)
+                    {
+                        if (!ModelState.IsValid)
+                        {
+                            ViewBag.Categories = _dbContext.Categories.ToList();
+                            return View("Create", viewModel);
+                        }
+
+                        var userId = User.Identity.GetUserId();
+                        var course = _dbContext.Courses.Single(c => c.Id == viewModel.Id && c.LecturerId == userId);
+
+                        course.Place = viewModel.Place;
+                        course.DateTime = viewModel.GetDateTime();
+                        course.CategoryId = viewModel.Category;
+
+                        _dbContext.SaveChanges();
+
+                        return RedirectToAction("Index", "Home");
+                    }
+        */
+
     }
 }
--- 1911065673_NguyenDucThong_BigSchool/Controllers/HomeController.cs
+++ 1911065673_NguyenDucThong_BigSchool/Controllers/HomeController.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿using _1911065673_NguyenDucThong_BigSchool.Models;
+using _1911065673_NguyenDucThong_BigSchool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
 namespace _1911065673_NguyenDucThong_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+            var viewModel = new CourseViewModel
+            {
+                UpcomingCourse = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
@@ -20,7 +38,7 @@ namespace _1911065673_NguyenDucThong_BigSchool.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult CreateCourse()
         {
             ViewBag.Message = "Your contact page.";
 
